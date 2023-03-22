88da0b72a7fd2f908b46142a6002729c2555a7c0
marcel0ll <marcelo@lotuz.dev>
2023-03-21 23:08:51 -0300

    feat: init project

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import vercel from '@astrojs/vercel/static';
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    analytics: true
+  }),
+  integrations: [mdx(), tailwind()]
+});
