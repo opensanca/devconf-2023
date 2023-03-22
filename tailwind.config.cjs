d19406d28f52cecc87de56a5dab0f26f4092343b
marcel0ll <marcelo@lotuz.dev>
2023-03-21 23:08:51 -0300

    change colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+  theme: {
+    extend: {
+        colors: {
+          'red': {
+            700: '#bc2649',
+          },
+      },
+    },
+  },
 	plugins: [],
 }
