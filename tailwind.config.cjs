a72ae3234f671b20531e43c54dc0c5c3b98796f9
marcel0ll <marcelo@lotuz.dev>
2023-03-21 23:08:51 -0300

    improve

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-        colors: {
-          'red': {
-            700: '#BE3455',
-            // 700: '#bc2649',
-          },
+      colors: {
+        'slate': {
+          900: '#1D2939',
+        },
+        'red': {
+          700: '#BE3455',
+          // 700: '#bc2649',
+        },
       },
     },
   },
-	plugins: [],
+  plugins: [],
 }
